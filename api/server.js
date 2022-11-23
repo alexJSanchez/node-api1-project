e9fc10ac2acb95ef4594c3bb599a941c63503f9b
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2022-11-23 11:06:12 -0500

    add get users

--- api/server.js
+++ api/server.js
@@ -4,4 +4,17 @@ const server = express();
 server.use(express.json());
 const User = require("./users/model");
 
+server.get("/api/users/", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "something went wrong",
+        err: err.message,
+      });
+    });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
