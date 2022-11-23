c22e20cb4b2b02c79948c2187ae52b7bcebbc7c5
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2022-11-23 11:08:07 -0500

    add post user

--- api/server.js
+++ api/server.js
@@ -17,4 +17,23 @@ server.get("/api/users/", (req, res) => {
     });
 });
 
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || !newUser.bio) {
+    res.status(400).json({
+      message: "provide name and bio",
+    });
+  } else {
+    User.insert(newUser)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: err.message,
+        });
+      });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
