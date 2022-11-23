5b8bce0d3acf579235cf8d98800de00e37f866e7
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2022-11-23 12:58:15 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -74,4 +74,29 @@ server.delete("/api/users/:id", (req, res) => {
     });
 });
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "does not exist",
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "provide name and bio",
+        });
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error updating user",
+      error: err.message,
+    });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
